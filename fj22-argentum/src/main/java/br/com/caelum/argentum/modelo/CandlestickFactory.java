2787884bc6f2f1c03a52879c4dfc31fb465ed4fc
lab6601 <lab6601@caelum144-05>
2017-01-30 22:03:41 -0200

    Argentum Project

---
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -0,0 +1,12 @@
+package br.com.caelum.argentum.modelo;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class CandlestickFactory {
+	
+	public Candlestick constroiCandleParaData(Calendar data, List<Negociacao> negociacoes){
+		return null;
+		
+		}
+}
